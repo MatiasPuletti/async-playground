127739caf962d0ecc579e0d4b7b32ed496e05c6e
Matias Puletti <matiaspuletti@Matiass-Laptop.local>
2024-06-11 21:47:18 +0200

    Overcoming callback hell and on to promises

---
+++ script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const renderCountry = function (data, className = ' ') {
+  const html = `<article class="country ${className}">
+<img class="country__img" src="${data.flag}" />
+<div class="country__data">
+  <h3 class="country__name">${data.name}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+    1
+  )} M people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+</div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html); // study
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = country => {
+  // AJAX call for country 1
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+  request.send(); // send request for data; which is fetched in the background. Once it's done it will emit the load event.
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText); // this = request; this.responseText returns JSON (a big string of text)
+    const [data] = JSON.parse(this.responseText); //JSON will be parsed into an array containing the objects (in this case just one); parse JSON string, destructure array
+    console.log(data);
+
+    // render country 1
+    renderCountry(data);
+
+    // Get neighbour country 2
+    const neighbour = data.borders?.[0];
+    console.log(`neighbours: ${data.borders}`);
+
+    const request2 = new XMLHttpRequest(); // need to make a new request object, as calling another method on the active request will abort any active operations
+    request2.open(
+      'GET',
+      `    https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+    );
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('netherlands');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+// const request = new XMLHttpRequest();
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`
+// );
+// request.send();
+
+const request = fetch(
+  'https://countries-api-836d.onrender.com/countries/name/argentina'
+);
+
+console.log(request);
