0942b2de908a4e9627d72e88f36fe76571bf1301
Matias Puletti <matiaspuletti@Matiass-Laptop.local>
2024-06-12 16:05:19 +0200

    Consuming promises

--- script.js
+++ script.js
@@ -22,57 +22,57 @@ const renderCountry = function (data, className = ' ') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = country => {
-  // AJAX call for country 1
-  const request = new XMLHttpRequest();
-  request.open(
-    'GET',
-    `https://countries-api-836d.onrender.com/countries/name/${country}`
-  );
-  request.send(); // send request for data; which is fetched in the background. Once it's done it will emit the load event.
+// const getCountryAndNeighbour = country => {
+//   // AJAX call for country 1
+//   const request = new XMLHttpRequest();
+//   request.open(
+//     'GET',
+//     `https://countries-api-836d.onrender.com/countries/name/${country}`
+//   );
+//   request.send(); // send request for data; which is fetched in the background. Once it's done it will emit the load event.
 
-  request.addEventListener('load', function () {
-    // console.log(this.responseText); // this = request; this.responseText returns JSON (a big string of text)
-    const [data] = JSON.parse(this.responseText); //JSON will be parsed into an array containing the objects (in this case just one); parse JSON string, destructure array
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     // console.log(this.responseText); // this = request; this.responseText returns JSON (a big string of text)
+//     const [data] = JSON.parse(this.responseText); //JSON will be parsed into an array containing the objects (in this case just one); parse JSON string, destructure array
+//     console.log(data);
 
-    // render country 1
-    renderCountry(data);
+//     // render country 1
+//     renderCountry(data);
 
-    // Get neighbour country 2
-    const neighbour = data.borders?.[0];
-    console.log(`neighbours: ${data.borders}`);
+//     // Get neighbour country 2
+//     const neighbour = data.borders?.[0];
+//     console.log(`neighbours: ${data.borders}`);
 
-    const request2 = new XMLHttpRequest(); // need to make a new request object, as calling another method on the active request will abort any active operations
-    request2.open(
-      'GET',
-      `    https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-    );
-    request2.send();
+//     const request2 = new XMLHttpRequest(); // need to make a new request object, as calling another method on the active request will abort any active operations
+//     request2.open(
+//       'GET',
+//       `    https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+//     );
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
 
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
 
-getCountryAndNeighbour('netherlands');
+// getCountryAndNeighbour('netherlands');
 
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 seconds passed');
-    setTimeout(() => {
-      console.log('3 seconds passed');
-      setTimeout(() => {
-        console.log('4 seconds passed');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 seconds passed');
+//       setTimeout(() => {
+//         console.log('4 seconds passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
 // const request = new XMLHttpRequest();
 // request.open(
@@ -86,3 +86,25 @@ const request = fetch(
 );
 
 console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+    });
+};
+
+getCountryData('portugal');
